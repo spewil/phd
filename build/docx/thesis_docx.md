2d5d40c83407686d68083aa7dc2439e19f1d0097
spewil <spencer@spewil.com>
2021-01-27 15:37:32 +0000

    Begin physiology section

--- build/docx/thesis_docx.md
+++ build/docx/thesis_docx.md
@@ -1,15 +1,12 @@
 ---
 title: Sensorimotor Learning in Virtual Environments
 author: Spencer R. Wilson
-date: 1/1/2021
-toc-title: Contents
+date: \today
 xnos-cleveref: true
 xnos-capitalise: true
+papersize: a4
+geometry: margin=30mm
 ---
-
-<h2>Where are you?</h2>
-
-This is an experiment in creating an open kind of thesis. To start adding comments to this page, just highlight some text, click `annotate` and start typing. Note that you will have to a <a href="https://web.hypothes.is/" target="_blank">Hypothes.is</a> account, but it only takes a moment (and it's a nonprofit organization). Add as many comments as you like!
 
 <!-- you MUST have new lines between transcludes! -->
 
@@ -27,4 +24,4 @@ $include ../../sections/theory.md
 
 $include ../../sections/next_steps.md -->
 
-# Bibliography
+## Bibliography
--- compile_thesis.py
+++ compile_thesis.py
@@ -10,11 +10,32 @@
 import pypandoc
 import click
 import os
+import pathlib
+
+root_folder = pathlib.Path.cwd()
+print(root_folder)
 
 
-def main():
-    os.chdir("build/html")
+@click.command()
+@click.option('-o', "--output_type", default=None, required=False, type=str)
+def main(output_type):
+    if output_type == "html":
+        compile_to_html()
+    elif output_type == "pdf":
+        compile_to_pdf()
+    elif output_type == "docx":
+        compile_to_docx()
+    elif output_type is None:
+        compile_to_html()
+        compile_to_pdf()
+        compile_to_docx()
+    else:
+        raise ValueError(f"Output type {output_type} not recognized")
 
+
+def compile_to_html():
+    print("compiling to html")
+    os.chdir(root_folder / "build/html/")
     pdoc_args = [
         '--toc',
         '--toc-depth=4',
@@ -29,59 +50,62 @@ def main():
         '--csl=../assets/nature.csl',
         '--mathml',  # this works in chrome and firefox
         "--css=pandoc.css",  # custom css
-        # "--include-before-body",
-        # "preamble.tex",
         "--include-in-header",  # custom header -- so I can add hypothesis, etc
         "header.html",
         "--template",
         "template.html",
         "--citeproc"
     ]
-
-    input_path = "thesis.md"
-    pypandoc.convert_file(input_path,
-                          'html5',
+    pypandoc.convert_file(source_file="thesis.md",
+                          to='html5',
                           outputfile="../../index.html",
                           format='md',
                           extra_args=pdoc_args)
 
 
-@click.command()
-@click.argument('input_file', type=str)
-@click.argument('output_type', default='html', type=str)
-def compile_to_pdf(input_file, output_type):
-    os.chdir("build/latex")
+def compile_to_pdf():
+    print("compiling to pdf")
+    os.chdir(root_folder / "build/latex")
     pdoc_args = [
         '--number-sections',
         '--filter',
         'pandoc-include',
         '--filter',
         'pandoc-xnos',
-        '--bibliography=bibliography.bib',
+        '--bibliography=../assets/bibliography.bib',
         '--metadata',
         'link-citations=true',  # make citation links
-        '--csl=nature.csl',
-        # "--include-before-body",
-        # "preamble.tex",
-        # '--template',
-        # "template",
+        '--csl=../assets/nature.csl',
         "--citeproc",
-        "--verbose"
     ]
-    # output_path = input_path.parent / Path("index.html")
-    pypandoc.convert_file(source_file=input_file,
+    pypandoc.convert_file(source_file="thesis_pdf.md",
                           format='md',
                           to='pdf',
-                          outputfile="thesis.pdf",
+                          outputfile="../../thesis.pdf",
+                          extra_args=pdoc_args)
+
+
+def compile_to_docx():
+    print("compiling to docx")
+    os.chdir(root_folder / "build/docx")
+    pdoc_args = [
+        '--number-sections',
+        '--filter',
+        'pandoc-include',
+        '--filter',
+        'pandoc-xnos',
+        '--bibliography=../assets/bibliography.bib',
+        '--metadata',
+        'link-citations=true',  # make citation links
+        '--csl=../assets/nature.csl',
+        "--citeproc",
+    ]
+    pypandoc.convert_file(source_file="thesis_docx.md",
+                          format='md',
+                          to="docx",
+                          outputfile="../../thesis.docx",
                           extra_args=pdoc_args)
 
 
 if __name__ == '__main__':
-    import sys
-    try:
-        sys.argv[1]
-        print(f"compiling {sys.argv[1]}")
-        compile_to_pdf()
-    except IndexError:
-        print("compiling to index.html")
-        main()
+    main()
